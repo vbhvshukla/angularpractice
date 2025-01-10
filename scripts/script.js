556173f07263601de7cbdb783b145b6e8698278b
Vaibhav <rushukla7@gmail.com>
2025-01-10 16:30:43 +0530

    Ng-Repeat & Ng-init

--- scripts/script.js
+++ scripts/script.js
@@ -14,12 +14,54 @@
 var myApp = angular
   .module("myModule", [])
   .controller("myController", function ($scope) {
-    var employee = {
-      firstName: "Vaibhav",
-      lastName: "Shukla",
-      age: "96",
-      profileImage: "/assets/1.jpg",
-    };
+    var employees = [
+      {
+        firstName: "Vaibhav",
+        lastName: "Shukla",
+        age: "12",
+      },
+      {
+        firstName: "Ben",
+        lastName: "Hastings",
+        age: "32",
+      },
+      {
+        firstName: "Sara",
+        lastName: "Paul",
+        age: "44",
+      },
+      {
+        firstName: "Mark",
+        lastName: "Holland",
+        age: "65",
+      },
+      {
+        firstName: "Pam",
+        lastName: "Macintosh",
+        age: "76",
+      },
+      {
+        firstName: "Todd",
+        lastName: "Barber",
+        age: "24",
+      },
+    ];
+    var countries = [
+        {
+            name : "UK",
+            cities : ["London","Birmingham","Manchester"]
+        },
+        {
+            name : "USA",
+            cities : ["Los Angeles","Chicago","Houston"]
+        },
+        {
+            name : "India",
+            cities : ["Hyderabad","Chennai","Mumbai"]
+        }
+
+    ]
     $scope.message = "Hello!";
-    $scope.employee = employee;
+    $scope.employees = employees;
+    $scope.countries = countries;
   });
