696526819b54ebf95f05b1e9ecb680faba48d762
Vaibhav <rushukla7@gmail.com>
2025-01-13 15:18:28 +0530

    AnchorScroll && Location services

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,8 @@
-// var myApp = angular.module("myModule", []);
-
-// myApp.controller("myController", function ($scope) {
-//     var employee = {
-//         firstName : "Vaibhav",
-//         lastName : "Shukla",
-//         age : "9696"
-//     }
-//     $scope.message = "Hello!";
-//     $scope.employee = employee;
-// });
-
-//Method chaining
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope, stringService) {
-    $scope.transformString = function(input){
-        $scope.output = stringService.processString(input);
+  .controller("myController", function ($scope,$location,$anchorScroll) {
+    $scope.scrollTo = function(scrollLocation){
+        $location.hash(scrollLocation);
+        $anchorScroll();
     }
   });
