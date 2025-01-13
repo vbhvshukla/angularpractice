92da35d21bc16224b1be5def247271fe5134e1ca
Vaibhav <rushukla7@gmail.com>
2025-01-13 21:38:28 +0530

    Added Routing

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,76 @@
 var myApp = angular
-  .module("myModule", [])
-  .controller(
-    "myController",
-    function ($scope, $http, $location, $anchorScroll) {
-      $scope.data = "";
-      (() => {
-        $http.get("https://api.github.com/users/vbhvshukla").then((res) => {
-          $scope.data = res.data;
-        
-        });
-      })();
-      
-      $scope.scrollTo = function(scrollLocation) {
-        $location.hash(scrollLocation);
-        $anchorScroll();
-        // Now you can use $scope.data if needed
-        console.log($scope.data);
-      };
-    }
-  );
+  .module("myModule", ["ngRoute"])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when("/home", {
+        templateUrl: "../components/home.html",
+        controller: "homeController",
+      })
+      .when("/courses", {
+        templateUrl: "../components/courses.html",
+        controller: "coursesController",
+      })
+      .when("/students", {
+        templateUrl: "../components/students.html",
+        controller: "studentsController",
+      })
+      .when("/students/:id", {
+        templateUrl: "../components/studentDetails.html",
+        controller: "studentsDetailsController",
+      })
+      .otherwise({
+        redirectTo: "/home"
+      });
+      // $locationProvider.html5Mode(true);
+  })
+  .controller("homeController", function ($scope) {
+    $scope.message = "Home Page";
+  })
+  .controller("coursesController", function ($scope) {
+    $scope.courses = ["C#", "VB.NET", "CPP", "ASP.NET"];
+  })
+  .controller("studentsController", function ($scope) {
+    $scope.students = [
+      {
+        id: 1,
+        name: "John Doe",
+        gender: "Male",
+        city: "New York",
+      },
+      {
+        id: 2,
+        name: "Jane Smith",
+        gender: "Female",
+        city: "Los Angeles",
+      },
+      {
+        id: 3,
+        name: "Michael Johnson",
+        gender: "Male",
+        city: "Chicago",
+      },
+      {
+        id: 4,
+        name: "Emily Davis",
+        gender: "Female",
+        city: "Houston",
+      },
+      {
+        id: 5,
+        name: "Chris Lee",
+        gender: "Male",
+        city: "San Francisco",
+      },
+      {
+        id: 6,
+        name: "Sophia Martinez",
+        gender: "Female",
+        city: "Miami",
+      },
+    ];
+  })
+  .controller("studentDetailsController",function($scope,$routeParams){
+    $scope.studentId = $routeParams.id;
+  })
+
+  
