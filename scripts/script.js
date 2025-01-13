e1e61f76e10416c6253939df65f75d45acac2aea
Vaibhav <rushukla7@gmail.com>
2025-01-13 17:05:13 +0530

    Anchor scroll and location aong with http req

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,21 @@
 var myApp = angular
   .module("myModule", [])
-  .controller("myController", function ($scope,$location,$anchorScroll) {
-    $scope.scrollTo = function(scrollLocation){
+  .controller(
+    "myController",
+    function ($scope, $http, $location, $anchorScroll) {
+      $scope.data = "";
+      (() => {
+        $http.get("https://api.github.com/users/vbhvshukla").then((res) => {
+          $scope.data = res.data;
+        
+        });
+      })();
+      
+      $scope.scrollTo = function(scrollLocation) {
         $location.hash(scrollLocation);
         $anchorScroll();
+        // Now you can use $scope.data if needed
+        console.log($scope.data);
+      };
     }
-  });
+  );
