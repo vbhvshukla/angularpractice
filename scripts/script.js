cd46eec05328ac7a06569caf51c81f6c23232bff
Vaibhav <rushukla7@gmail.com>
2025-01-13 12:34:18 +0530

    Custom filter function

--- scripts/script.js
+++ scripts/script.js
@@ -13,20 +13,6 @@
 //Method chaining
 var myApp = angular
   .module("myModule", [])
-  .filter("gender",function(){
-    return function(gender){
-        switch(gender) {
-            case 1:
-                return "Male";
-            case 2:
-                return "Female";
-            case 3:
-                return "Not disclosed";
-            default:
-                return "Unknown";
-        }
-    }
-  })
   .controller("myController", function ($scope) {
     // var employees = [
     //   {
