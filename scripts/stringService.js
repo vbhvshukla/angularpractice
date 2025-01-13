deac4c202a390c793d808417e74c33a2d82c4228
Vaibhav <rushukla7@gmail.com>
2025-01-13 14:35:21 +0530

    Custom Service

---
+++ scripts/stringService.js
@@ -0,0 +1,15 @@
+myApp.factory("stringService", function () {
+  return {
+    processString: function (input) {
+      if (!input) return input;
+      let output = "";
+      for (let i = 0; i < input.length; i++) {
+        if (i > 0 && input[i] == input[i].toUpperCase()) {
+          output += " ";
+        }
+        output += input[i];
+      }
+      return output;
+    },
+  };
+});
